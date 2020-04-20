2843f4db151160322c60bc531ce0f68878761978
Gord West <52170149+Westabago@users.noreply.github.com>
2020-04-19 19:06:58 -0600

    msg

--- create.bat
+++ create.bat
@@ -1,8 +1,7 @@
 @echo off
 
 If "%1"=="" (
-    echo "Enter a project name!"
-) 
-else ( 
+    echo Enter a project name!
+) else ( 
     python "C:/Users/gordi/Desktop/CODE/GitHub/AutoGitHubRepo/create.py" %1
 )
